04a306439179484bcc0b8ad4e0e68a0de5ec0b70
Cesar M <cesarmardueno.b@gmail.com>
2023-07-18 23:03:04 -0600

    firmware and block diagram of the project

---
+++ Firmware/A10102_V1.0/A10102_V1.0/main.c
@@ -0,0 +1,18 @@
+/*
+ * A10102_V1.0.c
+ *
+ * Created: 18/07/2023 10:51:41 p. m.
+ * Author : Cesar M
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
